5f17e468c0bc28b6f4a67892a3184036984febc4
Mwangi Gikonyo <mwangi.gikonyo.t@gmail.com>
2024-05-14 17:58:27 +0300

    Removed port for baseUrl, production configs

--- config/env/production.js
+++ config/env/production.js
@@ -269,7 +269,7 @@ module.exports = {
     //   'https://example.com',
     //   'https://staging.example.com',
     // ],
-    onlyAllowOrigins: ['http://146.190.19.227:1337', 'http://localhost:1337']
+    onlyAllowOrigins: ['http://146.190.19.227:1337']
 
     /***************************************************************************
     *                                                                          *
@@ -355,7 +355,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  port: 1337,
+  // port: 1337,
 
 
 
