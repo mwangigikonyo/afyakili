54e9eb4b48f6a4d398f9d5a342cbcc43ecd91e5d
Mwangi Gikonyo <mwangi.gikonyo.t@gmail.com>
2024-05-14 17:48:07 +0300

    Updated env for production

--- config/env/production.js
+++ config/env/production.js
@@ -389,7 +389,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'http://146.190.19.227:1337',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
